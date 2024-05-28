678934a84cec9ebf36148a60c31476a6d5367b1e
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-29 01:17:33 +0530

    Update UserContext.jsx

--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 // Context provides a way to pass data through the component tree without having to pass props down
-//  manually at every leve
+//  manually at every level
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
